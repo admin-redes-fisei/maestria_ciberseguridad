f200eda2c04bbfcb21f6b0f63937baeb6db649ce
alisonMSalas <asalas2854@uta.edu.ec>
2024-05-15 15:55:33 -0500

    Se agrego el botón en proceso de admisión, se corrigieron las faltas ortografícas y se modificó la resolución

--- js/script.js
+++ js/script.js
@@ -1075,6 +1075,17 @@
 			}
 		}
 
+		document.getElementById("btn-proceso").addEventListener("click", function() {
+        
+			var pdfUrl = "files/req.pdf";
+	
+			var link = document.createElement("a");
+			link.href = pdfUrl;
+			link.download = "proceso_selección.pdf"; 
+			document.body.appendChild(link);
+			link.click();
+			document.body.removeChild(link); 
+		});
 
 		/**
 		 * Select2
